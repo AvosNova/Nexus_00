155e5d7a9ba891a4dad8deb0b0ce78811c5bbdf1
staticBytess <160567397+staticBytess@users.noreply.github.com>
2024-04-01 11:15:24 -0700

    Added search to urlpatterns

--- pantryPal/data_handler/urls.py
+++ pantryPal/data_handler/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_recipes
+from .views import get_recipes , process_search_view
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('recipes/', get_recipes, name='get_recipes'),
+     path('search/', process_search_view, name='process_search'),
 ]
